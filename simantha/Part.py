bcff46b35a86f96358daf9b4d975e8b700d4a3fe
mehdidadfarnia <mahdid@gmail.com>
2021-03-25 09:54:33 -0400

    Update Part.py
    testing another update on 3/25/21

--- simantha/Part.py
+++ simantha/Part.py
@@ -1,7 +1,24 @@
 class Part:
     def __init__(self, id_):
         self.id_ = id_
-        self.routing_history = []
+  #      self.routing_history = []
 
     def initialize(self):
         pass
+
+
+
+ #   def initialize(self):
+ #       self.quality = 1.0
+
+
+
+
+#    def __init__(self, id_, initial_quality=1):
+ #       self.id_ = id_
+#        self.routing_history = []
+#        self.initial_quality=initial_quality
+#        self.quality = initial_quality
+
+#    def initialize(self):
+#        self.quality = self.initial_quality 
