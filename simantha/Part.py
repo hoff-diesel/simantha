3bf484b388f894cd0cbd24bed19859d13ab77be0
mehdidadfarnia <mahdid@gmail.com>
2021-03-25 10:18:35 -0400

    Update Part.py

--- simantha/Part.py
+++ simantha/Part.py
@@ -1,10 +1,10 @@
-class Part:
-    def __init__(self, id_):
-        self.id_ = id_
+#class Part:
+#    def __init__(self, id_):
+ #       self.id_ = id_
   #      self.routing_history = []
 
-    def initialize(self):
-        pass
+  #  def initialize(self):
+   #     pass
 
 
 
